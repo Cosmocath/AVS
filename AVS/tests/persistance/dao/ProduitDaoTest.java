365d0a027352ed157e8a7b9b46bd312b553ded33
Noranouna <noraliferki@hotmail.fr>
2019-09-06 15:25:20 +0200

    BeforeEach PDT_00 chargement des JDD

--- AVS/tests/persistance/dao/ProduitDaoTest.java
+++ AVS/tests/persistance/dao/ProduitDaoTest.java
@@ -2,10 +2,18 @@ package persistance.dao;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
+import java.util.Scanner;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.NativeQuery;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import persistance.factory.HibernateFactory;
 import persistance.produit.beanDo.ProduitDo;
 import persistance.produit.dao.IProduitDao;
 import util.Factory;
@@ -18,6 +26,31 @@ import util.OrderBy;
  *
  */
 class ProduitDaoTest {
+    
+    /**
+     * 
+     */
+    @BeforeEach
+    public void initData() {
+        try (final Session session = HibernateFactory.getSessionFactory().openSession()) {
+            final Transaction transaction = session.beginTransaction();
+            // on lit le fichier
+            try (final Scanner scanner = new Scanner(new FileReader("tests/dataSet/avs_DML.sql"))) {
+                while (scanner.hasNext()) {
+                    final String sql = scanner.nextLine();
+                    // pour chaque ligne non vide
+                    if (!sql.isEmpty()) {
+                        // on l'exécute en tant que query native (sql natif)
+                        final NativeQuery<?> query = session.createNativeQuery(sql);
+                        query.executeUpdate();
+                    }
+                }
+            } catch (final FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            transaction.commit();
+        }
+    }
 
     /**
      * Test method for {@link persistance.produit.dao.impl.ProduitDao#findAllProduitOrderBy(util.OrderBy)}.
