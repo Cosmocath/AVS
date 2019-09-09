54b20fb02d1b8fee28a3f212ae0b5a5d274f876b
houcine59 <houcaf@gmail.com>
2019-09-09 09:59:14 +0200

    tache DeconnecterUser

---
+++ AVS/src/presentation/users/action/DeconnecterUserAction.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package presentation.users.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @author Administrateur
+ *
+ */
+public class DeconnecterUserAction extends Action {
+
+    @Override
+    public ActionForward execute(final ActionMapping mapping, final ActionForm form, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        final HttpSession session = request.getSession();
+        session.removeAttribute(ConnecterUserAction.USER_CONNECTED);
+        session.invalidate();
+        return mapping.findForward("success");
+    }
+}
