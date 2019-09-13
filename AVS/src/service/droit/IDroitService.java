2b18180e9c483b291d262fafad0896ac056348d2
fossi59 <fossipena@hotmail.fr>
2019-09-13 14:14:38 +0200

    suppression message conflit dans le fichier IDroitService

--- AVS/src/service/droit/IDroitService.java
+++ AVS/src/service/droit/IDroitService.java
@@ -11,11 +11,7 @@ import java.util.Map;
  */
 public interface IDroitService {
     /**
-<<<<<<< HEAD
      * permet de recupérer la liste des droits selon les profils
-=======
-     * TODO XSI : compléter + formater
->>>>>>> a9f766e4777d9e948b391e77d52206312605927c
      * 
      * @return une map qui prend en clé une url, et en valeur une liste de profil qui a accès à l'url
      */
