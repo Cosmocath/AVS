d169d8b0a88204fb4000a284823a114610444dad
houcine59 <houcaf@gmail.com>
2019-09-26 13:17:11 +0200

    message panierVide

--- AVS/src/presentation/panier/action/AjouterPanierAction.java
+++ AVS/src/presentation/panier/action/AjouterPanierAction.java
@@ -8,8 +8,6 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
-import org.apache.struts.action.ActionMessages;
 
 import presentation.panier.beanDto.PanierDto;
 import presentation.users.action.ConnecterUserAction;
@@ -35,15 +33,6 @@ public class AjouterPanierAction extends Action {
         //je récupère le panier en session et je le remplis
         final PanierDto panierDto = (PanierDto) session.getAttribute(ConnecterUserAction.MON_PANIER);
         iPanierService.addProduitPanier(panierDto, idProduit);
-
-        //message panier vide
-        if (panierDto == null) {
-            final ActionMessages messages = new ActionMessages();
-            messages.add("panierVide", new ActionMessage("panier Vide"));
-            saveMessages(request, messages);
-        }
-        //
-
         final String retour = request.getParameter("retour");
         // je convertis ma chaine retour en une valeur de l enum
         final ParamPanier valeurEnum = ParamPanier.getValue(retour);
--- AVS/src/presentation/panier/action/VoirPanierAction.java
+++ AVS/src/presentation/panier/action/VoirPanierAction.java
@@ -2,11 +2,17 @@ package presentation.panier.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
+
+import presentation.panier.beanDto.PanierDto;
+import presentation.users.action.ConnecterUserAction;
 
 /**
  * Permet d'afficher le panierDto de la session
@@ -18,6 +24,14 @@ public class VoirPanierAction extends Action {
 
     @Override
     public ActionForward execute(final ActionMapping mapping, final ActionForm form, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+        final HttpSession session = request.getSession();
+        final PanierDto panierDto = (PanierDto) session.getAttribute(ConnecterUserAction.MON_PANIER);
+        //message panier vide
+        if (panierDto.getMapDesProduitsQte().size() == 0) {
+            final ActionMessages messages = new ActionMessages();
+            messages.add("panierVide", new ActionMessage("PAN_00.panierVide"));
+            saveMessages(request, messages);
+        }
         return mapping.findForward("success");
     }
 
